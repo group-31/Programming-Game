79ee3b94a2bc987599eb97661ae12b819c3a99fa
djchampion99 <djchampion99@gmail.com>
2019-02-26 15:52:50 +0000

    Added Game Manager
    Game manager will deal with level loading, keeping score and anything else that can't be handled by the player alone.

---
+++ Assassin's Code/Assets/Scripts/dontDestroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dontDestroy : MonoBehaviour
+{
+
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject); //Ensures object isn't destroyed between levels
+    }
+
+}
---
+++ Assassin's Code/Assets/Scripts/gameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void LoadScene(int x)
+    {
+        SceneManager.LoadScene(x); //Loads level specified by input
+    }
+
+}
