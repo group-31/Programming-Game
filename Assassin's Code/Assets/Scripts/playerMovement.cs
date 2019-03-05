5248309c2c27de047fdf2db4114cf71dd9e74322
djchampion99 <djchampion99@gmail.com>
2019-03-05 16:12:28 +0000

    Finished Player Moveset (wait needs work)
    Will add compiler later to activate these moves.

---
+++ Assassin's Code/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    void Start()
+    {
+        Move(1);
+        Turn(90);
+        Wait(2);
+        Move(2);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Move(int n)
+    {
+        transform.position += transform.up * n;
+    }
+
+    public void Turn(int n)
+    {
+        transform.Rotate(0,0,-n);
+    }
+
+    public void Wait(float n) //needs work
+    {
+        while (n > 0)
+        {
+            n -= Time.deltaTime;
+            Debug.Log(".");
+        }
+        return;
+    }
+
+}
