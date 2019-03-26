5f2f9ff844e401d2dfd8546cd607d0d01fb91de1
blobman264 <32684802+blobman264@users.noreply.github.com>
2019-03-26 14:37:12 +0000

    Assassin turns with look

--- Assassin's Code/Assets/Scripts/playerMovement.cs
+++ Assassin's Code/Assets/Scripts/playerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
 
+    public Sprite assassin, assassinBehind, assassinLeftSide, assassinRightSide;
+
     void Start()
     {
         transform.position = new Vector3(0, 0, 0);
@@ -15,16 +17,40 @@ public class playerMovement : MonoBehaviour
     {
         if(CheckPos() && n > 0) transform.position += transform.up;
         if(CheckPos() && n < 0) transform.position -= transform.up;
+        //this.GetComponent<SpriteRenderer>().sprite = assassin;
     }
 
     public void Turn(int n)
     {
         transform.Rotate(0,0,-n);
+        //Debug.Log(Mathf.RoundToInt(transform.rotation.ToEulerAngles().z));
+        changePlayerLook(transform.rotation.ToEulerAngles().z);
     }
 
     private bool CheckPos()
     {
         return true; //checks for walls etc before moving, to be added in future
+    }
+
+    public void changePlayerLook(float z)
+    {
+        z = Mathf.RoundToInt(z);
+        if (z == 2)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = assassinLeftSide;
+        }
+        else if (z == -2)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = assassinRightSide;
+        }
+        else if (z == 0)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = assassinBehind;
+        }
+        else
+        {
+            this.GetComponent<SpriteRenderer>().sprite = assassin;
+        }
     }
 
 }
