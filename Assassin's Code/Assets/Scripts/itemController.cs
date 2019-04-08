9d2fc525440ff49e286c71cb4ff7efb354421f49
djchampion99 <djchampion99@gmail.com>
2019-04-08 10:59:21 +0100

    Revert "Items in UI"
    This reverts commit a611ad65735f47d3ff2dd89aff6ec809a4a30ad1.

--- Assassin's Code/Assets/Scripts/itemController.cs
+++ Assassin's Code/Assets/Scripts/itemController.cs
@@ -1,38 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class itemController : MonoBehaviour
 {
     public int currentCoins;
-    public Text coinText;
-
     public bool keyOwned;
-    public Text keyText;
 
     void Start()
     {
         currentCoins = 0;
         keyOwned = false;
-
-        coinText.text = "coins = " + currentCoins + ";";
-
-        keyText.text = "key = " + keyOwned + ";";
-
     }
 
     public void coinPickup()
     {
         currentCoins++;
-
-        coinText.text = "coins = " + currentCoins + ";";
     }
 
     public void keyPickup()
     {
         keyOwned = true;
-
-        keyText.text = "key = " + keyOwned + ";";
     }
 }
